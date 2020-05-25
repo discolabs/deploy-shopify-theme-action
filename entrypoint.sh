e480e6cbfeb61cd5dc6152830e42407cfc656fcc
Gavin Ballard <gavin@gavinballard.com>
2020-05-25 17:16:42 +1000

    Add some debugging and additional checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,19 @@ theme configure --store=$INPUT_STORE --password=$INPUT_PASSWORD  --themeid=$INPU
 theme download assets/deploy || true
 LAST_DEPLOY_SHA=$(cat "$INPUT_PATH/assets/deploy")
 
+echo "---> LAST_DEPLOY_SHA is $LAST_DEPLOY_SHA"
+
 # Check if we could retrieve a deploy SHA.
-if [ ! -z "$LAST_DEPLOY_SHA" ]
-then
-  # Deploy only the files that have changed in our Shopify theme since the last deploy.
-  git diff $LAST_DEPLOY_SHA --name-only | xargs theme deploy $INPUT_ADDITIONAL_ARGS
+if [ ! -z "$LAST_DEPLOY_SHA" ]; then
+  # Get a list of files that have changed in our Shopify theme since the last deploy.
+  CHANGED_FILES=$(git diff "$LAST_DEPLOY_SHA..$GITHUB_SHA" --name-only)
+
+  echo "---> CHANGED_FILES is $CHANGED_FILES"
+
+  # Deploy only those changes, if they exist.
+  if [ ! -z "$LAST_DEPLOY_SHA" ]; then
+    echo "$CHANGED_FILES" | xargs theme deploy $INPUT_ADDITIONAL_ARGS
+  fi
 else
   # Deploy all files.
   theme deploy $INPUT_ADDITIONAL_ARGS
@@ -19,4 +27,4 @@ fi
 
 # Upload the latest deploy SHA.
 echo $GITHUB_SHA > "$INPUT_PATH/assets/deploy"
-theme deploy "$INPUT_PATH/assets/deploy"
+theme deploy assets/deploy

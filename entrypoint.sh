f6d0064649e3eb1f9856c9833546cf71b84945f0
Gavin Ballard <gavin@gavinballard.com>
2020-05-25 16:44:47 +1000

    Stamp Git SHA

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
-theme deploy --store=$INPUT_STORE --password=$INPUT_PASSWORD  --themeid=$INPUT_THEME_ID --dir=$INPUT_PATH $INPUT_ADDITIONAL_ARGS
+theme configure --store=$INPUT_STORE --password=$INPUT_PASSWORD  --themeid=$INPUT_THEME_ID --dir=$INPUT_PATH
+
+theme deploy $INPUT_ADDITIONAL_ARGS
+
+echo $GITHUB_SHA > "$INPUT_PATH/assets/deploy"
+theme deploy $INPUT_ADDITIONAL_ARGS "$INPUT_PATH/assets/deploy"

4b45e7ee4023e322fd4e07e5ce82996e0586d1f1
Gavin Ballard <gavin@gavinballard.com>
2020-05-25 17:02:22 +1000

    Use the last deploy SHA to minimise unneeded deploys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh -l
 
+# Configure Theme Kit.
 theme configure --store=$INPUT_STORE --password=$INPUT_PASSWORD  --themeid=$INPUT_THEME_ID --dir=$INPUT_PATH
 
-theme deploy $INPUT_ADDITIONAL_ARGS
+# Fetch the deploy hash from the theme, if it exists and ignoring a file not found error.
+theme download assets/deploy || true
+LAST_DEPLOY_SHA=$(cat "$INPUT_PATH/assets/deploy")
 
+# Check if we could retrieve a deploy SHA.
+if [ ! -z "$LAST_DEPLOY_SHA" ]
+then
+  # Deploy only the files that have changed in our Shopify theme since the last deploy.
+  git diff $LAST_DEPLOY_SHA --name-only | xargs theme deploy $INPUT_ADDITIONAL_ARGS
+else
+  # Deploy all files.
+  theme deploy $INPUT_ADDITIONAL_ARGS
+fi
+
+# Upload the latest deploy SHA.
 echo $GITHUB_SHA > "$INPUT_PATH/assets/deploy"
-theme deploy $INPUT_ADDITIONAL_ARGS "$INPUT_PATH/assets/deploy"
+theme deploy "$INPUT_PATH/assets/deploy"
